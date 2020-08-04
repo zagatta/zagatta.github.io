f2d12ac2f384e3ae9b252a65d415a55051eeb2b0
Jonas Zagatta <zagatta@sonah.tech>
2020-08-04 22:55:44 +0200

    all done except books

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 $(document.write('<meta charset="UTF-8">\n<meta name="viewport" content="width=device-width, initial-scale=1">\n<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">\n<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">\n<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\n<link rel="stylesheet" href="styles.css">'));
 $(document).ready(function(){
     $("#header").load("header.html");
+    $("#footer").load("footer.html");
 });
 
